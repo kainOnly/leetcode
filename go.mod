272f2174fde7a9a6881f1f2163eaa4d05f04cc94
kain <zhangtqx@vip.qq.com>
2020-03-29 14:18:15 +0800

    初始化项目

--- go.mod
+++ go.mod
@@ -1,9 +1,9 @@
 module redis-microservice
 
-go 1.13
+go 1.14
 
 require (
-	github.com/go-redis/redis/v7 v7.0.0-beta.4 // indirect
-	github.com/golang/protobuf v1.3.2
-	google.golang.org/grpc v1.26.0
+	github.com/go-redis/redis/v7 v7.2.1-0.20200327134818-9a49a4d91df7
+	github.com/golang/protobuf v1.3.5
+	google.golang.org/grpc v1.29.0-dev.0.20200326222940-e965f2a60b15
 )
--- main.go
+++ main.go
@@ -1,19 +1,4 @@
 package main
 
-import (
-	"google.golang.org/grpc"
-	"log"
-	"net"
-	"redis-microservice/controller"
-	pb "redis-microservice/router"
-)
-
 func main() {
-	listen, err := net.Listen("tcp", "127.0.0.1:6060")
-	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
-	}
-	server := grpc.NewServer()
-	pb.RegisterRouterServer(server, controller.New())
-	server.Serve(listen)
 }
