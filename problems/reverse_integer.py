8100700e45345e76a5c0481b1529133944edb4fd
kain <zhangtqx@vip.qq.com>
2021-05-07 21:27:23 +0800

    update

--- problems/reverse_integer.py
+++ problems/reverse_integer.py
@@ -1,18 +1,22 @@
 class Solution:
     def reverse(self, x: int) -> int:
-        Int32Min, Int32Max = -2147483648, 2147483647
-        if x == 0 or x < Int32Min or x > Int32Max:
+        def offset(n: int) -> bool:
+            return n < -2147483648 or n > 2147483647
+
+        if x == 0 or offset(x):
             return 0
+
         signed = x < 0
         if signed:
             x = -x
+
         val = 0
         while x != 0:
             val *= 10
             val += x % 10
             x //= 10
+
         if signed:
             val = -val
-        if val < Int32Min or val > Int32Max:
-            return 0
-        return int(val)
+
+        return 0 if offset(val) else int(val)
