c5dd9deb0750e261fcb40f8130874f74f58e8ae1
kain <zhangtqx@vip.qq.com>
2021-05-09 10:42:11 +0800

    update

--- problems/longest_palindromic_substring.py
+++ problems/longest_palindromic_substring.py
@@ -1,39 +1,31 @@
-# TODO:待优化
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        palindrome = s[0]
-        length, cursor = len(s), 0
+        length, palindrome = len(s), s[0]
         if length < 2:
             return palindrome
+
+        def findPalindrome(i: int, offset: int) -> str:
+            section, expand = '', 1
+            while True:
+                rindex, lindex = i + offset - expand, i + expand
+                if rindex < 0 or lindex > length - 1:
+                    break
+                right, left = s[rindex], s[lindex]
+                if right == left:
+                    sec = s[rindex:lindex + 1]
+                    if len(sec) > len(section):
+                        section = sec
+                    expand += 1
+                else:
+                    break
+            return section
+
+        cursor = 0
         while cursor < length:
-            expand1, expand2 = 1, 1
-            # odd
-            while True:
-                rindex = cursor - expand1
-                lindex = cursor + expand1
-                if rindex < 0 or lindex > length - 1:
-                    break
-                right, left = s[rindex], s[lindex]
-                if right == left:
-                    sec = s[rindex:lindex + 1]
-                    if len(sec) > len(palindrome):
-                        palindrome = sec
-                    expand1 += 1
-                else:
-                    break
-            # even
-            while True:
-                rindex = cursor + 1 - expand2
-                lindex = cursor + expand2
-                if rindex < 0 or lindex > length - 1:
-                    break
-                right, left = s[rindex], s[lindex]
-                if right == left:
-                    sec = s[rindex:lindex + 1]
-                    if len(sec) > len(palindrome):
-                        palindrome = sec
-                    expand2 += 1
-                else:
-                    break
+            str1 = findPalindrome(cursor, 0)
+            str2 = findPalindrome(cursor, 1)
+            p = str1 if len(str1) > len(str2) else str2
+            if len(p) > len(palindrome):
+                palindrome = p
             cursor += 1
         return palindrome
