1e47ee56a1d2737813f5945f73bb959bf012e322
kain <zhangtqx@vip.qq.com>
2021-06-06 15:39:27 +0800

    update

--- problems/best_time_to_buy_and_sell_stock.py
+++ problems/best_time_to_buy_and_sell_stock.py
@@ -1,12 +1,11 @@
+import math
 from typing import List
 
 
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        num = 0
-        for i in range(0, len(prices)):
-            for j in range(i + 1, len(prices)):
-                profit = prices[j] - prices[i]
-                if prices[j] - prices[i] > num:
-                    num = profit
-        return num
+        profit, lowest = 0, math.inf
+        for price in prices:
+            profit = max(price - lowest, profit)
+            lowest = min(price, lowest)
+        return profit
