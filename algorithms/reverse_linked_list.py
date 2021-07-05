11a6a9a8560cc80630b7d4f61323c37a25fdcc00
kain <zhangtqx@vip.qq.com>
2021-07-05 09:51:43 +0800

    更新

--- algorithms/reverse_linked_list.py
+++ algorithms/reverse_linked_list.py
@@ -18,13 +18,11 @@ class ListNode:
 
 class Solution:
     def reverseList(self, head: ListNode) -> ListNode:
-        data = []
+        reverse = None
         while head:
-            data.append(head.val)
+            cursor = ListNode(head.val)
+            if reverse:
+                cursor.next = reverse
+            reverse = cursor
             head = head.next
-        reverse = ListNode(-1)
-        cursor = reverse
-        while data:
-            cursor.next = ListNode(data.pop())
-            cursor = cursor.next
-        return reverse.next
+        return reverse
